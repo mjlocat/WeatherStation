b89ab242c98ebd34e46087c8d7a19927477c0b06
Michael Locatelli <mjlocat@gmail.com>
2021-03-07 12:38:25 -0800

    Send SIGINT to processes when restarting so they clean up properly

--- checkrunning.sh
+++ checkrunning.sh
@@ -5,10 +5,10 @@ DBPID=`ps x | grep writetodb.py | grep -v grep | awk '{ print $1 }'`
 
 if [ -z "${WSPID}" -o -z "${DBPID}" ]; then
   if [ ! -z "${WSPID}"]; then
-    kill ${WSPID} 2>/dev/null
+    kill -INT ${WSPID} 2>/dev/null
   fi
   if [ ! -z "${DBPID}" ]; then
-    kill ${DBPID} 2>/dev/null
+    kill -INT ${DBPID} 2>/dev/null
   fi
   ./weatherstation 2>/dev/null | python3 writetodb.py &
 fi
